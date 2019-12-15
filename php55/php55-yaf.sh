85e7dc5b255704f30ee5a22488fb45c86131fa6f
mjc199252 <806606676@qq.com>
2019-12-16 01:34:24 +0800

    添加5.5版本yaf的config悉尼下

--- php55/php55-yaf.sh
+++ php55/php55-yaf.sh
@@ -3,4 +3,5 @@ wget "https://pecl.php.net/get/yaf-2.3.3.tgz"
 tar -zxvf "yaf-2.3.3.tgz"
 cd yaf-2.3.3
 phpize
-./configure --with-php-config=/php-config
+./configure --with-php-config=/usr/local/php/php55/5.5.38_1/bin/php-config
+make && make install
